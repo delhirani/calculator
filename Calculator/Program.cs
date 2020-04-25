5143bffa37d39820786016c72dccc35d37e14a00
Mahaveer Bathmaraju <Mahaveer.Bathmaraju@byojet.com>
2020-04-25 11:49:31 +1000

    commit

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,6 +10,42 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            string FirstName, LastName;
+            //char valueConverter = 'a';
+            //string FirstName = "rani", LastName = "mahaveer";
+            //Console.WriteLine((int)valueConverter);
+            //Console.WriteLine("My full name is {0} {1}", FirstName, LastName);
+            //Console.ReadLine(); 
+            //FirstName=Console.ReadLine();
+            //LastName= Console.ReadLine();
+            //Program ps = new Program();
+            //Console.ReadLine();
+            //ps.addName(FirstName, LastName);
+            FullName();
+            Console.ReadLine();
+
+        }
+        //void addName(string firstname, string lastname)
+        //{
+        //    Console.ReadLine("Enter the value for the first name", firstname);
+        //    Console.ReadLine("Enter the value for the first name", lastname);
+        //    Console.WriteLine("My full name is {0} {1}", firstname, lastname);
+        //}
+        //static void FullName(string firstname, string lastname)
+        //{
+        //    Console.ReadLine("Enter the value for the first name", firstname);
+        //    Console.ReadLine("Enter the value for the first name", lastname);
+        //    Console.WriteLine("My full name is {0} {1}", firstname, lastname);
+        //}
+
+        static void FullName( )
+        {
+            //Console.ReadLine("Enter the value for the first name", firstname);
+            //Console.ReadLine("Enter the value for the first name", lastname);
+             string firstname = Console.ReadLine();
+             string lastname = Console.ReadLine();
+            Console.WriteLine("My full name is {0} {1}", firstname, lastname);
+            Console.ReadLine();
         }
     }
 }
