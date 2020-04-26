4fa2015df4745c844951c27084dc606e314a47ea
Mahaveer Bathmaraju <Mahaveer.Bathmaraju@byojet.com>
2020-04-26 13:53:30 +1000

    Changes to trignometery class

---
+++ Calculator.Tests/TrigonometricUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Mathematics.Test
+{
+    [TestClass]
+    public class TrigonometricUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //First  
+            Trigonometric.TrianlgeSidesInput(10, 10, 15);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Second Test  
+            Trigonometric.TrianlgeSidesInput(10.5, 10.5, 15);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Thisrd Test  
+            Trigonometric.TrianlgeSidesInput(10.5, 12.5, 15.5);
+
+        }
+    }
+}
